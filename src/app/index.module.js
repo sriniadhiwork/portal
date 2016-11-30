a424d2a757fab3803a784875154f1227ea354a7b
Andrew Larned <andrew.larned@gmail.com>
2016-11-30 09:19:07 -0500

    PULSE-283: "Hide" editing of ACF, add popup, split ACFs if name shouldn't be edited

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
     'use strict';
 
     angular
-        .module('portal', ['ngAnimate', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ngIdle', 'ui.bootstrap', 'prettyXml', 'smart-table', 'swaggerUi', 'ngXslt', 'cb.x2js', 'angular-filters', 'ordinal', 'googlechart', 'portal.common', 'portal.constants', 'portal.main', 'portal.review', 'portal.search', 'portal.resources']);
+        .module('portal', ['ngAnimate', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ngIdle', 'ui.bootstrap', 'prettyXml', 'smart-table', 'swaggerUi', 'ngXslt', 'cb.x2js', 'angular-filters', 'ordinal', 'googlechart', 'angular-confirm', 'portal.common', 'portal.constants', 'portal.main', 'portal.review', 'portal.search', 'portal.resources']);
 
 })();
