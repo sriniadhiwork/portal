fd6db3d6b157887e051cb011a80ff8e0095aebb9
Andrew Larned <andrew.larned@gmail.com>
2017-06-21 08:33:44 -0400

    PULSE-486: Removed 'raw' document view

--- src/app/review/components/document_review/document_review.directive.js
+++ src/app/review/components/document_review/document_review.directive.js
@@ -3,15 +3,16 @@
 
     angular
         .module('portal.review')
-        .directive('aiDocumentReview', aiDocumentReview);
+        .directive('aiDocumentReview', aiDocumentReview)
+        .controller('DocumentReviewController', DocumentReviewController);
 
     /** @ngInject */
-    function aiDocumentReview($log) {
+    function aiDocumentReview() {
         var directive = {
             restrict: 'E',
             templateUrl: 'app/review/components/document_review/document_review.html',
             scope: {},
-            controller: DocumentReviewController,
+            controller: 'DocumentReviewController',
             controllerAs: 'vm',
             bindToController: {
                 activeDocument: '=?'
@@ -19,48 +20,47 @@
         };
 
         return directive;
+    }
 
-        /** @ngInject */
-        function DocumentReviewController($filter, $scope, commonService) {
-            var vm = this;
+    /** @ngInject */
+    function DocumentReviewController($filter, $log, $scope, $window, commonService) {
+        var vm = this;
 
-            vm.cancel = cancel;
-            vm.displayName = commonService.displayName;
+        vm.cancel = cancel;
+        vm.displayName = commonService.displayName;
 
-            activate();
+        activate();
 
-            ////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////
 
-            function activate () {
-                vm.transformedDocument = '';
-                vm.displayType = 'xslt';
-                vm.xslt = loadXMLDoc('assets/xslt/CDA_Style.xsl');
-                $scope.$watch('vm.activeDocument', function (newDoc) {
-                    if (newDoc) {
-                        vm.transformedDocument = $filter('xslt')(newDoc.data, vm.xslt);
-                    }
-                });
+        function activate () {
+            vm.transformedDocument = '';
+            vm.xslt = loadXMLDoc('assets/xslt/CDA_Style.xsl');
+            $scope.$watch('vm.activeDocument', function (newDoc) {
+                if (newDoc) {
+                    vm.transformedDocument = $filter('xslt')(newDoc.data, vm.xslt);
+                }
+            });
+        }
+
+        function cancel () {
+            vm.activeDocument = undefined;
+            vm.transformedDocument = '';
+        }
+
+        ////////////////////////////////////////////////////////////////////
+
+        function loadXMLDoc (filename) {
+            var xhttp;
+            if ($window.ActiveXObject) {
+                xhttp = new $window.ActiveXObject('Msxml2.XMLHTTP');
+            } else {
+                xhttp = new XMLHttpRequest();
             }
-
-            function cancel () {
-                vm.activeDocument = undefined;
-                vm.transformedDocument = '';
-            }
-
-            ////////////////////////////////////////////////////////////////////
-
-            function loadXMLDoc (filename) {
-                var xhttp;
-//                if ($window.ActiveXObject) {
-//                    xhttp = new $window.ActiveXObject('Msxml2.XMLHTTP');
-//                } else {
-                    xhttp = new XMLHttpRequest();
-//                }
-                xhttp.open('GET', filename, false);
-                try { xhttp.responseType = 'msxml-document' } catch(err) { $log.debug(err) } // Helping IE11
-                xhttp.send('');
-                return xhttp.responseText;
-            }
+            xhttp.open('GET', filename, false);
+            try { xhttp.responseType = 'msxml-document' } catch(err) { $log.debug(err) } // Helping IE11
+            xhttp.send('');
+            return xhttp.responseText;
         }
     }
 })();
