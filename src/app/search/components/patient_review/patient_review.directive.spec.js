60a0b6bf3beb56baabdcb4a016f87ef13e960414
Andrew Larned <andrew.larned@gmail.com>
2017-06-26 11:13:10 -0400

    PULSE-494: Requery workflow changed to allow editing of terms

--- src/app/search/components/patient_review/patient_review.directive.spec.js
+++ src/app/search/components/patient_review/patient_review.directive.spec.js
@@ -270,32 +270,10 @@
                 expect(vm.requery).toBeDefined();
             });
 
-            it('should call commonService.searchForPatient when requeried', function () {
-                var terms = Mock.queries[0].terms;
+            it('should broadcast the query when called', function () {
+                spyOn($rootScope, '$broadcast');
                 vm.requery(Mock.queries[0]);
-                expect(commonService.searchForPatient).toHaveBeenCalledWith(terms);
-            });
-
-            it('should refresh local queries when requeried', function () {
-                spyOn(vm,'getQueries');
-                vm.requery(Mock.queries[0]);
-                el.isolateScope().$digest();
-                expect(vm.getQueries).toHaveBeenCalled();
-            });
-
-            it('should call clearQuery to clear the requeried query', function () {
-                spyOn(vm,'clearQuery');
-                vm.requery(Mock.queries[0]);
-                expect(vm.clearQuery).toHaveBeenCalledWith(Mock.queries[0]);
-            });
-
-            it('should clear the query from patientQueries immediately', function () {
-                var newQueries = angular.copy(Mock.queries);
-                newQueries.splice(1,1);
-                commonService.getQueries.and.returnValue($q.when(newQueries));
-                vm.requery(Mock.queries[1]);
-                el.isolateScope().$digest();
-                expect(vm.patientQueries.length).toBe(Mock.queries.length - 1);
+                expect($rootScope.$broadcast).toHaveBeenCalledWith('requery', {terms: Mock.queries[0].terms});
             });
 
             it('should have a function to requery individual organizations', function () {
